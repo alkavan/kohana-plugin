c59e2655b724b763619c38622def88f1eca190e5
Igal Alkon <igal.alkon@gmail.com>
2011-07-08 10:37:56 +0300

    removed unneeeded code

--- views/plugin_template.php
+++ views/plugin_template.php
@@ -2,32 +2,10 @@
 <!DOCTYPE html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<meta name="keywords" content="<?php if ( ! empty($meta_keys)) echo $meta_keys ?>" />
-	<meta name="description" content="<?php if ( ! empty($meta_desc)) echo $meta_desc ?>" />
-	<!-- Google WMT -->
-	<meta name="verify-v1" content="bWrRhsmz40Fiqk4zaOp2ai+Sgk5+Ob/mvl5qpQKmn+k=" />
-	<title><?php if( ! empty($meta_title)): ?><?php echo $meta_title." - ".Kohana::config('app')->site_name ?><?php else: ?><?php echo Kohana::config('app')->site_name ?><?php endif ?></title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<title>plugin</title>
 	<script type="text/javascript">var base_url = '<?php echo url::base() ?>';</script>
-<?php
-// template css, javascript, meta data
-if (isset($styles))  foreach ($styles  as $style)  echo HTML::style($style, array('media'=>'screen'), TRUE), "\n";
-if (isset($scripts)) foreach ($scripts as $script) echo HTML::script($script), "\n";
-if (isset($metas))   foreach ($metas   as $meta)   echo HTML::meta($meta), "\n";
-?>
-
-<?php if (IN_PRODUCTION): ?>
-<script type="text/javascript">
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', '<?php echo Kohana::config('app')->analytics ?>']);
-	_gaq.push(['_trackPageview']);
-
-	(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-</script>
-<?php endif ?>
 </head>
 
 <body class="">
